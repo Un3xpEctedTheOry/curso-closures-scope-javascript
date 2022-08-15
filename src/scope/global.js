c79264b79696bcb50729a8a79784a9f53b478525
Un3xpEctedThe0ry <monteromarrufoandremartin@gmail.com>
2022-08-14 21:45:27 -0500

    global scope

---
+++ src/scope/global.js
@@ -0,0 +1,22 @@
+// variables
+var a // declarando
+var b = 'b' // declarando y asignando
+b = 'bb' // reasignación
+var a = 'aa' // redeclaración
+
+// global scope
+var fruit = 'apple' // global
+
+function bestFruit() {
+    console.log(fruit)
+}
+
+bestFruit()
+
+function countries(){
+    country = 'colombia'; // declaramos como global (sin var)
+    console.log(country)
+}
+
+countries()
+console.log(country) // se accede a la variable fuera de la función
