7abb4bc1cb8230d3cedb74752682e9715346ebd0
Un3xpEctedThe0ry <monteromarrufoandremartin@gmail.com>
2022-08-16 13:10:30 -0500

    debugging

---
+++ src/debug/index.js
@@ -0,0 +1,10 @@
+// pegamos el código en el navegador (pestaña about:blank)
+var a = 'Hello'
+
+function hello() {
+    let b = 'Hello, world'
+    const c = 'Hello, world!'
+    debugger // pausa la ejecucion y abre junto a la consola el depurador (source)
+}
+
+hello()
